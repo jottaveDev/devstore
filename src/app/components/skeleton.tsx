749530cc1e18d61ebcf61a3bb7fc9731c2385ae6
João Victor <joao_victorflamengo10@outlook.com>
2024-10-24 20:57:29 -0300

    feat: add home component

---
+++ src/app/components/skeleton.tsx
@@ -0,0 +1,13 @@
+import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+const Skeleton = ({ className, ...props }: ComponentProps<'div'>) => {
+  return (
+    <div
+      className={twMerge('bg-zinc-50/10 animate-pulse rounded-md', className)}
+      {...props}
+    />
+  )
+}
+
+export default Skeleton
