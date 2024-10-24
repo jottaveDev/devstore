d878579aff71ab8ee688f4eabe25c7249f84ddcd
João Victor <joao_victorflamengo10@outlook.com>
2024-10-24 20:58:22 -0300

    feat: add helper function to convert price

---
+++ src/app/utils/convertPrice.ts
@@ -0,0 +1,8 @@
+export function convertPriceToBRL(price: number) {
+  return price.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  })
+}
