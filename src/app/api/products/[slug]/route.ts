cd73e5e9a0edb2335e5a6e65531042cd1889a74f
João Victor <joao_victorflamengo10@outlook.com>
2024-10-24 20:51:30 -0300

    feat: add API

---
+++ src/app/api/products/[slug]/route.ts
@@ -0,0 +1,17 @@
+import data from '../data.json'
+
+interface ProductParamsProps {
+  params: Promise<{
+    slug: string
+  }>
+}
+
+export async function GET(_: Request, { params }: ProductParamsProps) {
+  const { slug } = await params
+
+  const product = data.products.find((product) => product.slug === slug)
+  if (!product) {
+    return Response.json({ message: 'Product not found.' }, { status: 400 })
+  }
+  return Response.json(product)
+}
