1ee36e8ab97c3a7c7c8464dd6966d3fd5dbdb441
João Victor <joao_victorflamengo10@outlook.com>
2024-10-24 21:00:48 -0300

    feat: add product component

---
+++ src/app/(store)/product/[slug]/page.tsx
@@ -0,0 +1,95 @@
+import { convertPriceToBRL } from '@/app/utils/convertPrice'
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+import Image from 'next/image'
+
+interface ProductParamsProps {
+  params: Promise<{
+    slug: string
+  }>
+}
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`)
+  const product = await response.json()
+  return product
+}
+
+const ProductPage = async ({ params }: ProductParamsProps) => {
+  const { slug } = await params
+  const product = await getProduct(slug)
+
+  return (
+    <div className="relative grid max-h=[860px] grid-cols-3">
+      <div className="col-span-2 overflow-hidden">
+        <Image
+          src={product.image}
+          alt={product.title}
+          width={1000}
+          height={1000}
+          quality={100}
+        />
+      </div>
+
+      <div className="flex flex-col justify-center px-12">
+        <h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
+
+        <p className="mt-2 leading-relaxed text-zinc-400">
+          {product.description}
+        </p>
+
+        <div className="mt-8 flex items-center gap-3">
+          <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
+            {convertPriceToBRL(product.price)}
+          </span>
+          <span className="text-sm text-zinc-400">
+            Em até 12x sem juros de{' '}
+            {(product.price / 12).toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            })}
+          </span>
+        </div>
+
+        <div className="mt-8 space-y-4">
+          <span className="block font-semibold">Tamanhos</span>
+          <div className="flex gap-2">
+            <button
+              type="button"
+              className="flex items-center justify-center h-9 w-14 rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+            >
+              P
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center h-9 w-14 rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+            >
+              M
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center h-9 w-14 rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+            >
+              G
+            </button>
+            <button
+              type="button"
+              className="flex items-center justify-center h-9 w-14 rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+            >
+              GG
+            </button>
+          </div>
+        </div>
+
+        <button
+          type="button"
+          className="mt-8 flex justify-center items-center h-12 rounded-full bg-emerald-600 font-semibold text-white"
+        >
+          Adicionar ao carrinho
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default ProductPage
