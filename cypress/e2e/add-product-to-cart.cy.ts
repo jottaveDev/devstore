71aaf8963a159ccdd4434234326de5ddad8e8e00
João Victor <joao_victorflamengo10@outlook.com>
2024-10-31 00:02:12 -0300

    test: ensure the cart works

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,55 @@
+describe('Cart', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('should open cart modal', () => {
+    cy.get('a[href^="/product"]').first().click()
+    cy.location('pathname').should('include', '/product')
+
+    cy.contains('Cart').click()
+    cy.contains('Meu Carrinho').should('exist')
+  })
+
+  it('should close cart modal', () => {
+    cy.get('a[href^="/product"]').first().click()
+    cy.location('pathname').should('include', '/product')
+
+    cy.contains('Cart').click()
+    cy.get('button').first().click()
+    cy.contains('Meu Carrinho').should('not.exist')
+  })
+
+  it('should be able to navigate to the product page and it to the cart', () => {
+    cy.get('a[href^="/product"]').first().click()
+    cy.location('pathname').should('include', '/product')
+
+    cy.contains('GG').click()
+
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('adicionado ao carrinho :)').should('exist')
+  })
+
+  it('should not add to cart without selecting size', () => {
+    cy.get('a[href^="/product"]').first().click()
+    cy.location('pathname').should('include', '/product')
+
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('adicionado ao carrinho :)').should('not.exist')
+    cy.contains('selecione um tamanho antes de adicionar ao carrinho').should(
+      'exist',
+    )
+  })
+
+  it('should be able to search for a product and add it to the cart', () => {
+    cy.get('input[name=q]').type('moletom').parent('form').submit()
+
+    cy.get('a[href^="/product"]').first().click()
+    cy.location('pathname').should('include', '/product')
+
+    cy.contains('GG').click()
+
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('adicionado ao carrinho :)').should('exist')
+  })
+})
